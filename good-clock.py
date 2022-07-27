a5f7ecb204ca37ade68639ebd64dc0c05e73ab2a
Luca Rhee <lucarhee@gmail.com>
2022-07-27 13:41:55 +0900

    - add print big digits function

--- good-clock.py
+++ good-clock.py
@@ -18,6 +18,36 @@ import os
 import sys
 
 
+def print_big_digits(hours, minutes, seconds, SEPERATOR='   ', BIG_SEPERATOR='      '):
+    output = ''
+    lines = 0
+    while lines < 7:
+        if len(hours) > 1:
+            output += Digits[int(hours[0])][lines] + SEPERATOR
+            output += Digits[int(hours[1])][lines] + BIG_SEPERATOR
+        else:
+            output += Digits[0][lines] + SEPERATOR
+            output += Digits[int(hours[0])][lines] + BIG_SEPERATOR
+
+        if len(minutes) > 1:
+            output += Digits[int(minutes[0])][lines] + SEPERATOR
+            output += Digits[int(minutes[1])][lines] + BIG_SEPERATOR
+        else:
+            output += Digits[0][lines] + SEPERATOR
+            output += Digits[int(minutes[0])][lines] + BIG_SEPERATOR
+
+        if len(seconds) > 1:
+            output += Digits[int(seconds[0])][lines] + SEPERATOR
+            output += Digits[int(seconds[1])][lines] + '\n'
+        else:
+            output += Digits[0][lines] + SEPERATOR
+            output += Digits[int(seconds[0])][lines] + '\n'
+
+        lines += 1
+
+    print(output)
+
+
 def clock():
     while True:
         os.system('clear')
@@ -25,76 +55,22 @@ def clock():
         hours = now.split(':')[0]
         minutes = now.split(':')[1]
         seconds = now.split(':')[2]
-
-        lines = 0;
-        output = '';
-        SEPERATOR = '  '
-        BIG_SEPERATOR = '      '
-        while lines < 7:
-            if int(hours) >= 10:
-                output += Digits[1][lines] + SEPERATOR
-                output += Digits[int(hours[1])][lines] + BIG_SEPERATOR
-            else:
-                output += Digits[int(hours[0])][lines] + BIG_SEPERATOR
-
-            if len(minutes) > 1:
-                output += Digits[int(minutes[0])][lines] + SEPERATOR
-                output += Digits[int(minutes[1])][lines] + BIG_SEPERATOR
-            else:
-                output += Digits[int(minutes[0])][lines] + BIG_SEPERATOR
-
-            if len(minutes) > 1:
-                output += Digits[int(seconds[0])][lines] + SEPERATOR
-                output += Digits[int(seconds[1])][lines] + '\n'
-            else:
-                output += Digits[int(seconds[0])][lines] + '\n'
-
-            lines += 1
-        print(output)
+        print_big_digits(hours, minutes, seconds)
         time.sleep(1)
+
 
 def countdown(total_time):
     # total_time: minutes
     total_time = int(total_time) * 60  # in seconds
     while True:
-        left_time = total_time
         os.system('clear')
+        left_time = total_time
         hours = str(left_time // 60 // 60)
-        left_time = left_time % 60 % 60
+        left_time = left_time - (int(hours) * 60 * 60)
         minutes = str(left_time // 60)
-        left_time = left_time % 60
+        left_time = left_time - (int(minutes) * 60)
         seconds = str(left_time)
-        # print(f"{hours}:{minutes}:{seconds}")
-
-        lines = 0;
-        output = '';
-        SEPERATOR = '  '
-        BIG_SEPERATOR = '      '
-        while lines < 7:
-            if len(hours) > 1:
-                output += Digits[int(hours[0])][lines] + SEPERATOR
-                output += Digits[int(hours[1])][lines] + BIG_SEPERATOR
-            else:
-                output += Digits[0][lines] + SEPERATOR
-                output += Digits[int(hours[0])][lines] + BIG_SEPERATOR
-
-            if len(minutes) > 1:
-                output += Digits[int(minutes[0])][lines] + SEPERATOR
-                output += Digits[int(minutes[1])][lines] + BIG_SEPERATOR
-            else:
-                output += Digits[0][lines] + SEPERATOR
-                output += Digits[int(minutes[0])][lines] + BIG_SEPERATOR
-
-            if len(seconds) > 1:
-                output += Digits[int(seconds[0])][lines] + SEPERATOR
-                output += Digits[int(seconds[1])][lines] + '\n'
-            else:
-                output += Digits[0][lines] + SEPERATOR
-                output += Digits[int(seconds[0])][lines] + '\n'
-
-            lines += 1
-
-        print(output)
+        print_big_digits(hours, minutes, seconds)
         time.sleep(1)
         total_time -= 1
 
